54017febc852b9902f7d29e55bb7300aea7db25f
zero_asus <zero@ASUS.>
2024-11-17 18:19:12 -0500

    feat():     - Imagene de productos cargadas author: Zero

--- components/productCard.js
+++ components/productCard.js
@@ -1,7 +1,7 @@
 export function productCard(item) {
     const cardContentBlog = `
         <figure>
-            <img src="/media/utils/default.jpeg" alt="imagen por defecto">
+            <img src="/media/productos/${item.id}.webp" alt="imagen para ${item.referencia}">
         </figure>
         <div class="producto-info">
             <p class="marca"> ${item.marca} </p>
